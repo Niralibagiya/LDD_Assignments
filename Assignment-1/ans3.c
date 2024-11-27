39c9bfa923c78ea7b03565d1f323c950335568d8
Nirali Bagiya <niralisoni1007@gmailcom>
2024-11-27 22:36:57 +0530

    assignment-4

--- Assignment-1/ans3.c
+++ Assignment-1/ans3.c
@@ -14,7 +14,7 @@ static int __init list_modules_init(void)
 
    
     list_for_each_entry(mod, THIS_MODULE->list.prev, list) {
-        printk(KERN_INFO "Module: %s, Size: %u, State: %s\n", mod->name,mod->core_size,mod->state == MODULE_STATE_LIVE ?"LIVE": mod->state == MODULE_STATE_COMING ? "COMING" : mod->state == MODULE_STATE_GOING ? "GOING" : "UNKNOWN");
+        printk(KERN_INFO "Module: %s, Size: %u, State: %s\n", mod->name,mod->data_size,mod->state == MODULE_STATE_LIVE ?"LIVE": mod->state == MODULE_STATE_COMING ? "COMING" : mod->state == MODULE_STATE_GOING ? "GOING" : "UNKNOWN");
     }
 
     return 0;
